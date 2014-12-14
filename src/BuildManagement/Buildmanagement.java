e66ade43cd482dd5d97e29d4d19d98a013edf71e
csak9393 <Elfi.Lo@student.uibk.ac.at>
2014-12-14 19:38:44 +0100

    Update Buildmanagement.java

--- src/BuildManagement/Buildmanagement.java
+++ src/BuildManagement/Buildmanagement.java
@@ -7,15 +7,15 @@ public class Buildmanagement
 	// if there is a string input the string will be splittet at every whitespace und printed
 	public static void main(String[] args) 
 	{
-  	  if(args.length != 0) 
-  	  {
-      	Iterable<String> news = Splitter.on(CharMatcher.BREAKING_WHITESPACE).split(args[0]);
-      	for(String dsa : news) 
-    	  {
-      	  System.out.println(dsa);
-	      }
-	    }
-	    else
-	    System.out.println("Nothing to split");
+	  	  if(args.length != 0) 
+	  	  {
+		      	Iterable<String> news = Splitter.on(CharMatcher.BREAKING_WHITESPACE).split(args[0]);
+		      	for(String dsa : news) 
+	    	  	{
+	      	  		System.out.println(dsa);
+		      	}
+		   }
+		   else
+		   System.out.println("Nothing to split");
 	}
 }	
